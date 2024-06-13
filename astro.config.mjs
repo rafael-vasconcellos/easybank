fa8ba322f061189d776829643f45c1f9795b8eae
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2024-06-13 01:17:11 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
-
 import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
 export default defineConfig({
   // Enable Solid to support Solid JSX components.
   integrations: [solid(), tailwind()],
-  base: 'easybank/'
+  base: 'easybank/', // base url for static files, configured for github pages
 });
